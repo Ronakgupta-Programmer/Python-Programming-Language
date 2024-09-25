f32d849ed73326b41fcf2e5d5b64373ad9221d8e
Ronakgupta-Programmer <ronakgupta2610@gmail.com>
2024-09-25 13:22:22 -0700

    Add Face Tracking File

---
+++ Computer Vision/Face_Tracking.py
@@ -0,0 +1,26 @@
+import cv2
+
+def face_detection():
+    cap = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        cv2.imshow("Face Detection", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    face_detection()
